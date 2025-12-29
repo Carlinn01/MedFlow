4428dc92e5b02902c73030304c2a1e0e87202966
Carlinn01 <carloseduardodalazenoliveira@gmail.com>
2025-12-29 20:44:13 -0300

    Getters e set add

--- src/main/java/com/medflow/model/Usuario.java
+++ src/main/java/com/medflow/model/Usuario.java
@@ -10,24 +10,79 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Column(nullable = false)
     private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     @Column(nullable = false, unique = true)
     private String email;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Column(nullable = false)
     private String senha;
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
     @ManyToOne
     @JoinColumn(name = "perfil_id")
     private Perfil perfil;
 
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+
     @ManyToOne
     @JoinColumn(name = "clinica_id")
     private Clinica clinica;
 
+    public Clinica getClinica() {
+        return clinica;
+    }
+
+    public void setClinica(Clinica clinica) {
+        this.clinica = clinica;
+    }
+
     private boolean ativo = true;
 
-    // getters e setters
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
 }
